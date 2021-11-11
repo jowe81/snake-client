34c63bd867663bbffdcd0c2bbc9b476dd9032fd6
Johannes Weber <johannes@drweber.de>
2021-11-10 18:49:53 -0800

    client.js, property-value shorthand

---
+++ client.js
@@ -0,0 +1,20 @@
+// establishes a connection with the game server
+const net = require("net");
+
+const connect = function () {
+  console.log("Connecting ...");
+
+  const conn = net.createConnection({
+    host: 'localhost', // IP address here,
+    port: 50541,// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log("Server: ", data);
+  });
+  return conn;
+};
+
+module.exports = { connect };
