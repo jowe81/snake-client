59d74623da70bce87c36afd8e845b5cc9ecc7645
Johannes Weber <johannes@drweber.de>
2021-11-10 18:41:28 -0800

    log server messages

--- play.js
+++ play.js
@@ -3,13 +3,15 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: 'localhost', // IP address here,
+    port: 50541,// PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  conn.on('data', (data) => {
+    console.log("Server: ", data);
+  });
   return conn;
 };
 
