b6682a48a470da7b0c4815b8d94064f9764d6e8c
Johannes Weber <johannes@drweber.de>
2021-11-11 08:32:49 -0800

    Added comments

--- play.js
+++ play.js
@@ -1,5 +1,9 @@
+//Get the client module to handle connection to server
 const { connect } = require('./client');
+//Get logging functionality
 const { cl } = require('./log');
+//Get the setupInput function to init UI
 const { setupInput } = require('./input');
 
+//Start the game: connect to the server and start the UI
 setupInput(connect());
