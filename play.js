2893ff2fd1834caafb4ddcba2405a5b579927be9
Johannes Weber <johannes@drweber.de>
2021-11-11 09:42:07 -0800

    Comment formatting

--- play.js
+++ play.js
@@ -1,11 +1,13 @@
-//Get the client module to handle connection to server
+// play.js: entry point into the app
+
+// Get the client module to handle connection to server
 const { connect } = require('./client');
-//Get logging functionality
+// Get logging functionality
 const { cl } = require('./log');
-//Get the setupInput function to init UI
+// Get the setupInput function to init UI
 const { setupInput } = require('./input');
-//Get cmdline arguments
+// Get cmdline arguments
 const args = require('./args');
 
-//Start the game: connect to the server and start the UI
+// Start the game: connect to the server and start the UI
 setupInput(connect(args.name));
