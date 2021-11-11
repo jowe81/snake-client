11870972155e5abdd366b6d559068ac322dbe027
Johannes Weber <johannes@drweber.de>
2021-11-10 19:38:04 -0800

    Experimenting with move

--- client.js
+++ client.js
@@ -5,9 +5,19 @@ const { cl, setPrefix } = require("./log");
 //Set default prefix for logging to "Server"
 setPrefix("Client");
 
+//Set our name on the server
 const sendName = (conn, name) => {
   cl(`Sending name to server (${name})`);
   conn.write(`Name: ${name}`);
+};
+
+//Send a move command
+const move = (conn, direction) => {
+  if (['up', 'down', 'left', 'right'].includes(direction)) {
+    //This is a valid direction - send the command
+    cl(`Sending the Move: ${direction} command`);
+    conn.write(`Move: ${direction}`);
+  }
 };
 
 const connect = function () {
@@ -24,6 +34,7 @@ const connect = function () {
   conn.on('connect', (data) => {
     cl("Connection established");
     sendName(conn,'JW');
+    setInterval(() => move(conn, 'up'), 100);
   });
 
   conn.on('data', (data) => {
