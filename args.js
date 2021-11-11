29dd8bada413dcf55750cee67b96aa245778dc23
Johannes Weber <johannes@drweber.de>
2021-11-11 08:44:04 -0800

    args module to read cmdline args

---
+++ args.js
@@ -0,0 +1,18 @@
+//Get the command line arguments
+const rawArgs = process.argv.slice(2);
+
+//Object that will hold arguments as key-value pairs
+const args = {};
+
+for (let i = 0; i < rawArgs.length; i += 2) {
+  //Extract key value pair and add it to the args object
+  if (rawArgs[i].substr(0,1) === "-" && rawArgs[i].length > 1) {
+    let newKey = rawArgs[i].substr(1);
+    let newValue = rawArgs[i + 1];
+    if (newValue) {
+      args[newKey] = newValue;
+    }
+  }
+}
+
+module.export = args;
