7a871120b471b08536bbe924cce3d4d5df6f32ca
Johannes Weber <johannes@drweber.de>
2021-11-11 08:57:11 -0800

    Added comments

--- client.js
+++ client.js
@@ -1,42 +1,50 @@
 // client.js: Manages the connection with the game server
 
-//Get configuration
+// Get configuration
 const { CONNECT_TO, PLAYER_INITIALS } = require("./constants");
+
 // Get TCP tools
 const net = require("net");
+
 // Get logging functions from log.js
 const { cl, setPrefix } = require("./log");
-//Set default prefix for logging
+
+// Set default prefix for logging
 setPrefix("Client");
 
-//Set our name on the server
+// Set our name on the server
 const sendName = (conn, name) => {
   cl(`Sending name/player initials to server: ${name}`);
   conn.write(`Name: ${name}`);
 };
 
+// Initialize connection to game server
 const connect = function (name) {
-  cl("Connecting ...");
+  cl(`Attempting to connect to ${CONNECT_TO.IP}:${CONNECT_TO.PORT}`);
 
   const conn = net.createConnection({
     host: CONNECT_TO.IP,  // IP address here,
     port: CONNECT_TO.PORT,// PORT number here,
   });
 
-  // interpret incoming data as text
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on('connect', (data) => {
+  // Connection attempt succeeded
+  conn.on('connect', () => {
     cl("Connection established");
+    // Set our name from command line argument (if present) or PLAYER_INITALS constant
     sendName(conn, name ? name : PLAYER_INITIALS);
   });
 
+  // Log incoming data to the console
   conn.on('data', (data) => {
     if (data[0] === '\n') {
       data = data.substr(1);
     }
     cl(data, "Server");
   });
+
   return conn;
 };
 
