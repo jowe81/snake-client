368a4e624a84cfa00b06ccb8963f9a5f47078d90
Johannes Weber <johannes@drweber.de>
2021-11-10 20:02:15 -0800

    setupInput, handleUserInput

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
+const { cl } = require('./log');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (data) => {
+  //Check for CTRL+C
+  if (data === '\u0003') {
+    cl("CTRL+C received - goodbye!", "Keyboard");
+    process.exit();
+  }
+  cl(data,"Keyboard");
+};
+
+setupInput();
 connect();
